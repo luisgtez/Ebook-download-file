9ab4546a32ee9c53b7da2331f40333ab645b6d42
Luis Gutierrez <luismarkgo@gmail.com>
2024-06-19 22:09:41 +0200

    Refactored

--- README.md
+++ README.md
@@ -13,6 +13,16 @@ Flask python server to download files from server.
 
 ## Usage
 1. Place your desired file to install in the root directory of the project, it must be a ".pdf" or ".epub" file and there must be only one file of any of these types.
+
+### Method 1: Run File
+2. Run the `app.py` file.
+    ```bash
+    python app.py
+    ```
+    This will start the application on `localhost` and port `5001`.
+2. Open your web browser and navigate to `<host>:<port>` to download the file. By default, navigate to `localhost:5001`.
+
+### Method 2: Flask Command - Setup Host and Port manually
 2. Start the application on your desired host and port.
     ```bash
     flask run --host=<host> --port=<port>
--- app.py
+++ app.py
@@ -1,32 +1,48 @@
-from flask import Flask, redirect, send_file
+from flask import Flask, redirect, send_file, render_template
 import os
+import sys
+
 app = Flask(__name__)
 
-@app.route("/alas")
-def redirigir_a_archivo():
-    url_externa = "https://ia904500.us.archive.org/9/items/1234567_202312/Alas_de_sangre_Rebecca_Yarros.pdf"
-    return redirect(url_externa)
 
 @app.route("/")
 def root():
-    print("Listando archivos en directorio actual: ", os.getcwd())
-    files = os.listdir()
-    files_epub_pdf = [x for x in files if x.endswith(".pdf") or x.endswith(".epub")]
-    if len(files_epub_pdf) != 1:
-        file =""
-        print("No se encontró epub o pdf unico. Enviando archivo:", file) 
-        return send_file(file, as_attachment=True)
-    
-    else:
-        file = files_epub_pdf[0]
-        print("Enviando único archivo: ", file)
-        return send_file(file, as_attachment=True)
+    files = get_file_epub_pdf(os.listdir())
+    file = files[0]
+    print("Enviando único archivo: ", file)
+    return send_file(file, as_attachment=True)
 
-# @app.route("/")
-# def root():
-#     return "Hola mundo"
+def check_unique_file_epub_pdf(files):
+    files_epub_pdf = get_file_epub_pdf(files)
+    if len(files_epub_pdf) != 1:
+        return False
+    return True
+
+
+def get_file_epub_pdf(files):
+    files_epub_pdf = [x for x in files if x.endswith(".pdf") or x.endswith(".epub")]
+    return files_epub_pdf
+
 
 if __name__ == "__main__":
-# flask run --host=192.168.1.223 --port=5001
-    app.run(debug=False, host='192.168.1.223', port=5001)
+    if check_unique_file_epub_pdf(os.listdir()):
 
+        print("---------------------------------------------------------------------------")
+        print("Se encontró un único archivo .pdf o .epub en el directorio actual:")
+        print("*  ", get_file_epub_pdf(os.listdir())[0])
+        print("Sirviendo archivo...")
+        print("---------------------------------------------------------------------------")
+
+        app.run(debug=False, host="0.0.0.0", port=5001)
+ 
+    else:
+        
+        print("---------------------------------------------------------------------------------------------------------------------")
+        print(
+            "Error: Se esperaba un único archivo .pdf o .epub en el directorio actual. Se encontraron los siguientes archivos: "
+        )
+        for file in get_file_epub_pdf(os.listdir()):
+            print("*  ", file)
+        print("---------------------------------------------------------------------------------------------------------------------")
+
+        sys.exit(1)
